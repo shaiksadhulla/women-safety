0b9f340b215553c8dba6b86e167bd08300b53d2d
shaiksadhulla <139620432+shaiksadhulla@users.noreply.github.com>
2024-05-07 12:22:56 +0530

    Add files via upload

---
+++ MachineLearning.py.py
@@ -0,0 +1,154 @@
+import tkinter
+from textblob import TextBlob
+from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from string import punctuation
+from nltk.corpus import stopwords
+from tkinter import filedialog
+
+main = tkinter.Tk()
+main.title("Analysis of Women Safety in Indian Cities Using Twitter data")
+main.geometry("1200x1200")
+
+global filename
+tweets_list = []
+clean_list = []
+global pos, neu, neg
+
+
+def tweetCleaning(doc):
+    tokens = doc.split()
+    table = str.maketrans('', '', punctuation)
+    tokens = [w.translate(table) for w in tokens]
+    tokens = [word for word in tokens if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if not w in stop_words]
+    tokens = [word for word in tokens if len(word) > 1]
+    tokens = ' '.join(tokens)
+    return tokens
+
+
+def upload():
+    global filename
+    filename = filedialog.askopenfilename(initialdir="dataset")
+    text.delete('1.0', END)
+    text.insert(END, filename + " loaded\n");
+
+
+def read():
+    tweets_list.clear()
+    train = pd.read_csv(filename, encoding='iso-8859-1')
+    for i in range(len(train)):
+        tweet = train.get_value(i, 'Text')
+        tweets_list.append(tweet)
+        text.insert(END, tweet + "\n")
+
+
+def clean():
+    text.delete('1.0', END)
+    clean_list.clear()
+    for i in range(len(tweets_list)):
+        tweet = tweets_list[i]
+        tweet = tweet.strip("\n")
+        tweet = tweet.strip()
+        tweet = tweetCleaning(tweet.lower())
+        clean_list.append(tweet)
+        text.insert(END, tweet + "\n")
+
+def machineLearning():
+    text.delete('1.0', END)
+    global pos, neu, neg
+    pos = 0
+    neu = 0
+    neg = 0
+    for i in range(len(clean_list)):
+        tweet = clean_list[i]
+        blob = TextBlob(tweet)
+        if blob.polarity <= 0.2:
+            neg = neg + 1
+            text.insert(END, tweet + "\n")
+            text.insert(END, "Predicted Sentiment : NEGATIVE\n")
+            text.insert(END, "Polarity Score      : " + str(blob.polarity) + "\n")
+            text.insert(END, '====================================================================================\n')
+        if blob.polarity > 0.2 and blob.polarity <= 0.5:
+            neu = neu + 1
+            text.insert(END, tweet + "\n")
+            text.insert(END, "Predicted Sentiment : NEUTRAL\n")
+            text.insert(END, "Polarity Score      : " + str(blob.polarity) + "\n")
+            text.insert(END, '====================================================================================\n')
+        if blob.polarity > 0.5:
+            pos = pos + 1
+            text.insert(END, tweet + "\n")
+            text.insert(END, "Predicted Sentiment : POSITIVE\n")
+            text.insert(END, "Polarity Score      : " + str(blob.polarity) + "\n")
+            text.insert(END, '====================================================================================\n')
+            
+            
+
+def graph():
+    label_X = []
+    category_X = []
+    text.delete('1.0', END)
+    text.insert(END,"Saftey Factor\n\n")
+    text.insert(END,'Positive : '+str(pos)+"\n")
+    text.insert(END,'Negative : '+str(neg)+"\n")
+    text.insert(END,'Neutral  : '+str(neu)+"\n\n")
+    text.insert(END,'Length of tweets  : '+str(len(clean_list))+"\n")
+    text.insert(END,'Positive : '+str(pos)+' / '+ str(len(clean_list))+' = '+str(pos/len(clean_list))+'%\n')
+    text.insert(END,'Negative : '+str(neg)+' / '+ str(len(clean_list))+' = '+str(neg/len(clean_list))+'%\n')
+    text.insert(END,'Neutral  : '+str(neu)+' / '+ str(len(clean_list))+' = '+str(neu/len(clean_list))+'%\n')
+    label_X.append('Positive')
+    label_X.append('Negative')
+    label_X.append('Neutral')
+    category_X.append(pos)
+    category_X.append(neg)
+    category_X.append(neu)
+
+    plt.pie(category_X,labels=label_X,autopct='%1.1f%%')
+    plt.title('Women Saftey & Sentiment Graph')
+    plt.axis('equal')
+    plt.show()
+
+font = ('times', 16, 'bold')
+title = Label(main, text='Analysis of Women Safety in Indian Cities Using Twitter data')
+title.config(bg='black', fg='white')
+title.config(font=font)
+title.config(height=3, width=120)
+title.place(x=0, y=5)
+
+font1 = ('times', 14, 'bold')
+uploadButton = Button(main, text="Upload Tweets Dataset", command=upload)
+uploadButton.place(x=50, y=100)
+uploadButton.config(font=font1)
+
+
+
+readButton = Button(main, text="Read Tweets", command=read)
+readButton.place(x=50, y=150)
+readButton.config(font=font1)
+
+cleanButton = Button(main, text="Tweets Cleaning", command=clean)
+cleanButton.place(x=210, y=150)
+cleanButton.config(font=font1)
+
+mlButton = Button(main, text="Run Machine Learning Algorithm", command=machineLearning)
+mlButton.place(x=400, y=150)
+mlButton.config(font=font1)
+
+
+graphButton = Button(main, text="Women Saftey Graph", command=graph)
+graphButton.place(x=730,y=150)
+graphButton.config(font=font1) 
+
+
+font1 = ('times', 12, 'bold')
+text = Text(main, height=25, width=150)
+scroll = Scrollbar(text)
+text.configure(yscrollcommand=scroll.set)
+text.place(x=10, y=200)
+text.config(font=font1)
+
+main.config(bg='coral')
+main.mainloop()
